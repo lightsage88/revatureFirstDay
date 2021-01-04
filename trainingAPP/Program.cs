82228278781c309e37d3aa553c3ce866eeb1d681
lightsage88 <adrian.e.rosales@gmail.com>
2021-01-04 13:44:14 -0800

    First Commit

---
+++ trainingAPP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace trainingAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am starting to learn C#");
+
+        }
+    }
+}
